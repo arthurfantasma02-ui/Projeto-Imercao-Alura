743e3f63b34bb4290eb48a8ed05b944a9fa000e4
arthur <arthurfantasma02@gmail.com>
2025-11-22 00:56:26 -0300

    aula 03

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+let cardContainer;
+
+
 let dados = [];
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("botao-busca");
 
-
-async function IniciarBusca() {
+async function carregarDados() {
     let resposta = await fetch ("data.json");
-    resposta = await resposta.json();
+    dados = await resposta.json();
 
-    console.log(resposta);
+    renderizarCards(dados);
 }; 
+
+function buscar() {
+    
+    const termoBusca = searchInput.value.trim().toLowerCase();
+
+    // Filtra o array 'dados'
+    const resultados = dados.filter(dado => 
+        dado.nome.toLowerCase().includes(termoBusca) ||
+        dado.descricao.toLowerCase().includes(termoBusca)
+    );
+    renderizarCards(resultados);
+}
+
+function renderizarCards(dados) {
+    cardContainer = document.querySelector(".card-container");
+    cardContainer.innerHTML = "";
+    for ( let dado of dados) {
+         let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+            <h2>${dado.nome}</h2>
+            <p>${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank" class="card-link">Saiba Mais</a>
+        `;
+        cardContainer.appendChild(article);
+    }
+}
+
+searchButton.addEventListener("click", buscar);
+
+
+searchInput.addEventListener("input", buscar);
+
+carregarDados();
